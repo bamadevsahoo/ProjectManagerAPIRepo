ac8758a07483984bd86f9429278bf203d52dba7c
bamadevsahoo <bamadev.dev@gmail.com>
2019-01-05 20:41:48 +0530

    added unit test and nbench performance test

---
+++ PerformanceTestNbench/PerformanceTest.cs
@@ -0,0 +1,68 @@
+﻿using BusinessLayer;
+using DataAccessLayer;
+using NBench;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PerformanceTestNbench
+{
+    public class PerformanceTest
+    {
+
+        private const string AddCounterName = "AddCounter";
+        private Counter addCounter;
+
+
+        private const int AcceptableMinAddThroughput = 20000;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            addCounter = context.GetCounter(AddCounterName);
+
+        }
+        [PerfBenchmark(RunMode = RunMode.Throughput, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.LessThan, AcceptableMinAddThroughput)]
+        public void GetUsersThroughput_ThroughputMode(BenchmarkContext context)
+        {
+            UserBusiness userBusiness = new UserBusiness();                  
+            List<User> user = userBusiness.GetUsers().ToList();
+            addCounter.Increment();
+        }
+        [PerfBenchmark(RunMode = RunMode.Throughput, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.LessThan, AcceptableMinAddThroughput)]
+        public void GetParentTaskThroughput_ThroughputMode(BenchmarkContext context)
+        {
+            ParentTaskBusiness parentTaskBusiness = new ParentTaskBusiness();                     
+            List<ParentTask> parentTask = parentTaskBusiness.GetParentTasks().ToList();
+            addCounter.Increment();
+        }
+        [PerfBenchmark(RunMode = RunMode.Throughput, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.LessThan, AcceptableMinAddThroughput)]
+        public void GetProjectThroughput_ThroughputMode(BenchmarkContext context)
+        {
+            ProjectBusiness projectBusiness = new ProjectBusiness();
+                       
+            List<Project> project = projectBusiness.GetProjects().ToList();
+            addCounter.Increment();
+        }
+        [PerfBenchmark(RunMode = RunMode.Throughput, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.LessThan, AcceptableMinAddThroughput)]
+        public void GetTasksThroughput_ThroughputMode(BenchmarkContext context)
+        {
+            TaskBusiness taskBusiness = new TaskBusiness();
+                           
+            List<Task> task = taskBusiness.GetTasks().ToList();
+            addCounter.Increment();
+        }
+
+        [PerfCleanup]
+        public void Cleanup(BenchmarkContext context)
+        {
+            //does nothing
+        }
+    }
+}
--- ProjectManagerAPI.Tests/ProjectManagerAPItest.cs
+++ ProjectManagerAPI.Tests/ProjectManagerAPItest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BusinessLayer;
+using DataAccessLayer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProjectManagerAPI.Tests
@@ -6,9 +8,86 @@ namespace ProjectManagerAPI.Tests
     [TestClass]
     public class ProjectManagerAPItest
     {
+      
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void GetUserTest()
+        {    //Arrange
+            UserBusiness userBusiness = new UserBusiness();       
+            //Act         
+            User user = userBusiness.GetUser(1);
+            //Assert
+            if(user!=null)
+            Assert.AreEqual(1,user.UserId);
+            else
+                Assert.IsNotNull(user);
         }
+        [TestMethod]
+        public void PostUserTest()
+        {    //Arrange
+            UserBusiness userBusiness = new UserBusiness();
+            User usObj = new User();
+            usObj.UserId = 0;
+            usObj.FirstName ="testUser";
+            usObj.LastName = "lastName";
+            usObj.EmpId = "emp001";
+      
+            //Act
+            usObj= userBusiness.PostUser(usObj);
+           
+            //Assert
+            Assert.AreNotEqual(0,usObj.UserId);
+        }
+        [TestMethod]
+        public void GetParentTaskTest()
+        {    //Arrange
+            ParentTaskBusiness parentTaskBusiness = new ParentTaskBusiness();
+            //Act         
+            ParentTask parentTask = parentTaskBusiness.GetParentTask(2);
+            //Assert
+            if (parentTask != null)
+                Assert.AreEqual(2, parentTask.ParentId);
+            else
+                Assert.IsNotNull(parentTask);
+        }
+
+        [TestMethod]
+        public void PostParentTaskTest()
+        {    //Arrange
+            ParentTaskBusiness parentTaskBusiness = new ParentTaskBusiness();
+            ParentTask parentTask = new ParentTask();
+            parentTask.ParentTask1 = "Parent task test";
+            //Act
+            parentTaskBusiness.PostParentTask(parentTask);
+
+            //Assert
+            Assert.AreNotEqual(0, parentTask.ParentId);
+        }
+
+        [TestMethod]
+        public void GetProjectTest()
+        {    //Arrange
+            ProjectBusiness projectBusiness = new ProjectBusiness();
+            //Act                  
+            Project project = projectBusiness.GetProject(4);
+            //Assert
+            if (project != null)
+                Assert.AreEqual(4, project.ProjectId);
+            else
+                Assert.IsNotNull(project);
+        }
+
+        [TestMethod]
+        public void GetTaskTest()
+        {    //Arrange
+            TaskBusiness taskBusiness = new TaskBusiness();
+            //Act                  
+            Task task = taskBusiness.GetTask(4);
+            //Assert
+            if (task != null)
+                Assert.AreEqual(4, task.TaskId);
+            else
+                Assert.IsNotNull(task);
+        }
+
     }
 }
